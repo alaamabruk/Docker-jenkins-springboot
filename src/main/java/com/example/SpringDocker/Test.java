9f154c59385fed7aa56593a7181073852d7c94d6
alaa.mabruk <alaamabrouk34@gmail.com>
2021-02-22 12:21:37 +0400

    last scend commit

--- src/main/java/com/example/SpringDocker/Test.java
+++ src/main/java/com/example/SpringDocker/Test.java
@@ -3,5 +3,7 @@ package com.example.SpringDocker;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello jenkins");
+        System.out.println("hello jenkins for scend time !!!");
+
     }
 }
